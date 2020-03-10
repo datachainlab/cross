83f0abd2abcab7afe63a41a515f3b30b86a2e1b3
Jun Kimura <junkxdev@gmail.com>
2020-03-10 17:02:48 +0900

    remove unnecessary comment

--- x/ibc/cross/internal/keeper/dispatcher.go
+++ x/ibc/cross/internal/keeper/dispatcher.go
@@ -5,9 +5,6 @@ import (
 	"github.com/datachainlab/cross/x/ibc/cross/internal/types"
 )
 
-/* TODO
-should return Store(Store?) instead of StoreKeys
-*/
 type ContractHandler interface {
 	Handle(ctx sdk.Context, contract []byte) (types.State, error)
 	GetState(ctx sdk.Context, contract []byte) (types.State, error)
