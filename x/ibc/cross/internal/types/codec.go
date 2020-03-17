69839373b2c8cd3cb0cd185de2ac919f0230a1df
Jun Kimura <junkxdev@gmail.com>
2020-03-17 19:00:08 +0900

    add AckDataCommit type to codec

--- x/ibc/cross/internal/types/codec.go
+++ x/ibc/cross/internal/types/codec.go
@@ -24,5 +24,6 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(PacketDataPrepare{}, "cross/PacketDataPrepare", nil)
 	cdc.RegisterConcrete(PacketDataPrepareResult{}, "cross/PacketDataPrepareResult", nil)
 	cdc.RegisterConcrete(PacketDataCommit{}, "cross/PacketDataCommit", nil)
+	cdc.RegisterConcrete(AckDataCommit{}, "cross/AckDataCommit", nil)
 	cdc.RegisterInterface((*OP)(nil), nil)
 }
