19f65ac4267bf51d8c7ad2bf2923144dfff41323
Jun Kimura <junkxdev@gmail.com>
2020-10-29 14:47:01 +0900

    add MsgInitiate definition

--- x/core/types/msgs.go
+++ x/core/types/msgs.go
@@ -1,1 +1,47 @@
 package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// msg types
+const (
+	TypeInitiate = "initiate"
+)
+
+var _ sdk.Msg = (*MsgInitiate)(nil)
+
+// Route implements sdk.Msg
+func (MsgInitiate) Route() string {
+	return RouterKey
+}
+
+// Type implements sdk.Msg
+func (MsgInitiate) Type() string {
+	return TypeInitiate
+}
+
+// ValidateBasic performs a basic check of the MsgInitiate fields.
+// NOTE: timeout height or timestamp values can be 0 to disable the timeout.
+func (msg MsgInitiate) ValidateBasic() error {
+	if msg.Sender == "" {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "missing sender address")
+	}
+	return nil
+}
+
+// GetSignBytes implements sdk.Msg. The function will panic since it is used
+// for amino transaction verification which IBC does not support.
+func (msg MsgInitiate) GetSignBytes() []byte {
+	panic("IBC messages do not support amino")
+}
+
+// GetSigners implements sdk.Msg
+func (msg MsgInitiate) GetSigners() []sdk.AccAddress {
+	valAddr, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{valAddr}
+}
