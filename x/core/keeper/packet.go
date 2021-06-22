adc2e021092e50c7a69edf13b6e3ee7bd5b56edd
Jun Kimura <jun.kimura@datachain.jp>
2021-06-22 10:57:17 +0900

    fix to use our acknowledgement type
    This is because currently solidity protobuf cannot support `oneof` in protobuf
    
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- x/core/keeper/packet.go
+++ x/core/keeper/packet.go
@@ -24,7 +24,7 @@ func (p Keeper) ReceivePacket(ctx sdk.Context, packet channeltypes.Packet) (*sdk
 	return res, ack, nil
 }
 
-func (p Keeper) ReceivePacketAcknowledgementfunc(ctx sdk.Context, packet channeltypes.Packet, ack packets.IncomingPacketAcknowledgement) (*sdk.Result, error) {
+func (p Keeper) ReceivePacketAcknowledgement(ctx sdk.Context, packet channeltypes.Packet, ack packets.IncomingPacketAcknowledgement) (*sdk.Result, error) {
 	ip, err := packets.UnmarshalIncomingPacket(p.m, packet)
 	if err != nil {
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized IBC packet type: %T: %v", packet, err)
